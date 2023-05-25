1af0679be51a4d43b3757ceedabbe5c4455d34e3
Mang <mangemmanuel07@gmail.com>
2023-05-25 21:40:06 +0800

    Completed Assignment

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,32 @@
 # your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_string):
+        self.email_string = email_string
+
+    def parse(self):
+        email_list = re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b', self.email_string)
+        parsed_emails = sorted(list(set(email_list)))
+
+        return parsed_emails
+
+
+
+
+# import re
+
+# class EmailAddressParser:
+#     def __init__(self, email_string):
+#         self.email_string = email_string
+
+#     def parse(self):
+#         email_list = self.email_string.split(',')
+#         parsed_emails = []
+
+#         for email in email_list:
+#             parsed_emails += [addr.strip() for addr in email.split() if addr.strip()]
+
+#         unique_emails = list(set(parsed_emails))
+#         sorted_emails = sorted(unique_emails)
+#         return sorted_emails
